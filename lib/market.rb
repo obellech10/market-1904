5d95892b3339aaad1885edbe8f3352f610b1d950
Oscar Bellechasse <obellech10@gmail.com>
2019-05-29 13:43:40 -0600

    Create method for adding vendors

--- lib/market.rb
+++ lib/market.rb
@@ -1,9 +1,13 @@
 class Market
   attr_reader :name,
               :vendors
-              
+
   def initialize(vendor_name)
     @name = vendor_name
     @vendors = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor 
   end
 end
