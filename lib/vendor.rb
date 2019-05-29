8d445177d1f1505edcb2fe5a4129f4a44cb1112d
Oscar Bellechasse <obellech10@gmail.com>
2019-05-29 13:21:29 -0600

    Create check_stock and stock methods

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,9 +1,18 @@
 class Vendor
   attr_reader :name,
               :inventory
-              
+
   def initialize(name)
     @name = name
     @inventory = {}
+    @stock = Hash.new(0)
+  end
+
+  def check_stock(item, qty=0)
+    @stock[item]
+  end
+
+  def stock(item, qty)
+    @stock[item] = qty
   end
 end
