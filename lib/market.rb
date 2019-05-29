650d5e88d1e37e3857cf366a92498763bc81c8b0
Oscar Bellechasse <obellech10@gmail.com>
2019-05-29 13:51:02 -0600

    Create method vendor_names

--- lib/market.rb
+++ lib/market.rb
@@ -8,6 +8,15 @@ class Market
   end
 
   def add_vendor(vendor)
-    @vendors << vendor 
+    @vendors << vendor
   end
+
+  def vendor_names
+    name = []
+    @vendors.find_all do |vendor|
+      name << vendor.name
+    end
+    name
+  end
+
 end
