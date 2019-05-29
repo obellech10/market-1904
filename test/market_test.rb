bdf4128a0b0214303f6d167129bbb3a49c067fd4
Oscar Bellechasse <obellech10@gmail.com>
2019-05-29 13:37:44 -0600

    Add test for attributes

--- test/market_test.rb
+++ test/market_test.rb
@@ -14,4 +14,9 @@ class MarketTest < MiniTest::Test
   def test_that_it_exists
     assert_instance_of Market, @market
   end
+
+  def test_attributes
+    assert_equal "South Pearl Street Farmers Market", @market.name
+    assert_equal [], @market.vendors
+  end
 end
