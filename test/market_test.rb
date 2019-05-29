b912666427c3833dbc4f4b099b6afce0e26c3eed
Oscar Bellechasse <obellech10@gmail.com>
2019-05-29 14:04:10 -0600

    Add test for vendors_that_sell method

--- test/market_test.rb
+++ test/market_test.rb
@@ -45,4 +45,12 @@ class MarketTest < MiniTest::Test
     assert_equal expected, @market.vendor_names
   end
 
+  def test_vendors_sell_certain_items
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_2)
+    @market.add_vendor(@vendor_3)
+
+    assert_equal [@vendor_1, @vendor_3], @market.vendors_that_sell("Peaches")
+    assert_equal [@vendor_2], @market.vendors_that_sell("Banana Nice Cream")
+  end
 end
