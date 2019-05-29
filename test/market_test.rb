1e19a8fcb3dfcaaf4438f87165665417854c1cd3
Oscar Bellechasse <obellech10@gmail.com>
2019-05-29 14:21:17 -0600

    Update check_stock and stock methods to remove redundant code

--- test/market_test.rb
+++ test/market_test.rb
@@ -53,4 +53,13 @@ class MarketTest < MiniTest::Test
     assert_equal [@vendor_1, @vendor_3], @market.vendors_that_sell("Peaches")
     assert_equal [@vendor_2], @market.vendors_that_sell("Banana Nice Cream")
   end
+
+  def test_that_market_returns_sorted_items_list
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_2)
+    @market.add_vendor(@vendor_3)
+
+    expected = ["Banana Nice Cream", "Peach-Raspberry Nice Cream", "Peaches", "Tomatoes"]
+    assert_equal expected, @market.sorted_item_list
+  end
 end
